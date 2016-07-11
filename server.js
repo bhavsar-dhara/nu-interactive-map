ec26569aae8471969d4a1e67ddd57abd01c11e88
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-07-10 20:19:20 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Dhara on 7/9/2016.
+ */
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+app.listen(port, ipaddress);
